18c2dff5dbe0e5619e4f1d6d5c67c5817cd8774c
CasJuan <castrojuanig25@gmail.com>
2024-10-27 21:22:00 -0300

    desestructurando los objetos, aprendiendo sobre interfaces

--- 01-bases/src/main.ts
+++ 01-bases/src/main.ts
@@ -5,7 +5,8 @@ import viteLogo from '/vite.svg'
 //import './bases/01-const-let';
 //import './bases/02-objet';
 //import './bases/03-arrays'
-import './bases/04-function';
+//import './bases/04-function';
+import './bases/05-deses-obj';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
