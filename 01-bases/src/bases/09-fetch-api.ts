ddd4117299de7fe0edded8912c8fcebf42f8108c
CasJuan <castrojuanig25@gmail.com>
2025-01-05 20:34:20 -0300

    [add] interfaces de type

--- 01-bases/src/bases/09-fetch-api.ts
+++ 01-bases/src/bases/09-fetch-api.ts
@@ -1,6 +1,10 @@
+import type { GIFResponse } from "../interfaces/gif.response";
+
 const apiKey = '0nkT8xdzw3P8Ivy9aS1rHMcrtgUJkJ4M';
 
 fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
     .then(resp => resp.json)
-    .then( (body) => console.log({data}))
+    .then((body:GIFResponse) => {
+        console.log(body.data.images.downsized_medium.url);
+    })
     .catch(err => console.log(err));
