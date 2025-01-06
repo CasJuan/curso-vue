c35b94fc81cb11060c7853f76e1e680fb9eb04fb
JuanCastro <jcastro@colegium.com>
2025-01-06 12:12:16 -0300

    [add] agrego mi prmier setup como variable reactiva para poder utilizar

--- 02- intro-vue/app.js
+++ 02- intro-vue/app.js
@@ -1,12 +1,30 @@
 
-const {createApp, ref} = Vue;
+const { createApp,  ref} = Vue;
 
 const app = createApp({
     template: `
-    <h1>Hola Mundo</h1>
-    <p>Desde app.js</p>
-    
-    `
+    <h1>{{message}}</h1>
+    <p>{{author}}</p>
+    `,
+
+    setup(){
+
+       const message  = ref ("I'm Batman" );
+
+       const author =  ref ('Bruce Wayne');
+
+       setTimeout(() => {
+        message.value = 'Hola soy Goku';
+        author.value = 'Goku'
+       },1000);
+
+        return {
+            message,
+            author
+        }
+
+    }
+
 });
 
 app.mount('#myApp');
