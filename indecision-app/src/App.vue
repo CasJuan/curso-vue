f39e03d6106e34c9421fc7f1011db63455aaa39f
CasJuan <castrojuanig25@gmail.com>
2025-01-08 21:36:44 -0300

    [add] componente tradicional de props

--- indecision-app/src/App.vue
+++ indecision-app/src/App.vue
@@ -3,12 +3,14 @@
     <hr>    
 
     <MyCounter :value="5" />
+    <MyCounterScript :value="5" />
 
 </template>
 
 
 <script lang="ts" setup>
 import MyCounter from './components/MyCounter.vue';
+import MyCounterScript from './components/MyCounterScript.vue';
 
 </script>
 
