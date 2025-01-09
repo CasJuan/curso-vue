d0be2ec9a79232c10f65f4cdb766146a76cefcc1
CasJuan <castrojuanig25@gmail.com>
2025-01-08 21:42:02 -0300

    [add] separar logica SFC

--- indecision-app/src/App.vue
+++ indecision-app/src/App.vue
@@ -2,8 +2,9 @@
     <h1>Mi primera app</h1>
     <hr>    
 
-    <MyCounter :value="5" />
-    <MyCounterScript :value="5" />
+    <MyCounter :value="1" />
+    <MyCounterScript :value="2" />
+    <MyCounterScript2 :value="3" />
 
 </template>
 
@@ -11,6 +12,7 @@
 <script lang="ts" setup>
 import MyCounter from './components/MyCounter.vue';
 import MyCounterScript from './components/MyCounterScript.vue';
+import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
 
 </script>
 
