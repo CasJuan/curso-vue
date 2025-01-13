eae1c06801f99971078bd00539c2eff44598ea33
CasJuan <castrojuanig25@gmail.com>
2025-01-13 12:11:55 -0300

    [add] utilizo vbind para mejorar la informacion de las props

---
+++ indecision-app/src/views/IndecisionView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import MessageBox from '../components/chat/MessageBox.vue';
+import ChatMessages from '../components/chat/ChatMessages.vue';
+import type { ChatMessage } from '@/interfaces/chat-message.interface';
+import { ref } from 'vue';
+
+const messages = ref<ChatMessage[]>([
+  {
+  id: new Date().getTime(),
+  message: "Hola mundo",
+  itsMine:true,
+  },
+  {
+  id: new Date().getTime(),
+  message: "quieres ir a tomar cafe?",
+  itsMine:true,
+  },
+  {
+  id: new Date().getTime() +1,
+  message: "no",
+  itsMine:false,
+  image:"https://yesno.wtf/assets/no/26-34b31d1f0777f70c61488f67a36576a9.gif"
+  },
+]);
+
+</script>
+
+<!-- Fuente: https://tailwindcomponents.com/component/chat-layout -->
+<template>
+    <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
+      <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+        <span>Mi</span>
+      </div>
+  
+      <ChatMessages :messages="messages"/>
+  
+      <MessageBox/>
+
+    </div>
+  </template>
