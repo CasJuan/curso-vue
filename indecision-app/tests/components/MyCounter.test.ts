48649e82c0d5879f135eb788f579136c8ddd2b13
CasJuan <castrojuanig25@gmail.com>
2025-01-13 16:53:40 -0300

    [add] config para el @

--- indecision-app/tests/components/MyCounter.test.ts
+++ indecision-app/tests/components/MyCounter.test.ts
@@ -1,13 +1,17 @@
 import { describe, test } from "vitest";
-import MyCounter from '../../src/components/MyCounter.vue';
+import MyCounter from '@/components/MyCounter.vue';
 import { mount } from '@vue/test-utils'
 
 describe('<MyCounter/>', () => {
     test ('shoul match snapshot', () => {
         
         const wrapper = mount(MyCounter, {
-            
+            props: {
+                value: 5,
+            }
         });
+
+        console.log(wrapper.html());
 
 
     });
